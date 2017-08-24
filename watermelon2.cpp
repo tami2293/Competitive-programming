0c8682887b9d5ec674b420b0f1219fe9d88ada8d
tami2293 <tgr_93@hotmail.com>
2017-08-23 22:28:43 -0300

    watermelon problem added

---
+++ watermelon2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int w;
+	cin >> w;
+	if (w%2 != 0 || w == 1 || w == 2) {
+		cout << "NO" << endl;
+	}
+	else {
+		cout << "YES" << endl;
+	}
+	
+	return 0;}
